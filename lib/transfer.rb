48d977c9a67a954c7cab68925e26a578a4f46c02
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-11-09 21:00:51 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,10 @@
 class Transfer
-attr_accessor :status 
+attr_accessor :status
 attr_reader :sender, :receiver, :amount
 
 
 def initialize(sender, receiver, amount)
-  @sender = sender 
+  @sender = sender
   @receiver = receiver
   @amount = amount
   @staus = "pending"
@@ -23,14 +23,14 @@ def execute_transaction
 else
 @status = "rejected"
 "Transaction rejected. Please check your account balance."
-end 
+end
 end
 
 def reverse_transfer
   if @status == "complete"
     @sender.balance += @amount
     @receiver.balance -= @amount
-    @status = "reversed" 
+    @status = "reversed"
   end
 end
 end
